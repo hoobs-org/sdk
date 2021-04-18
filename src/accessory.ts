33f148a86baf2320f3f658d183aaee496e2c63cb
mkellsy <mkellsy@gmail.com>
2021-04-18 12:00:21 -0600

    remove blank image

--- src/accessory.ts
+++ src/accessory.ts
@@ -19,15 +19,6 @@
 import Request from "axios";
 import Config from "./config";
 
-const BLANK_IMAGE = [
-    "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAABhWlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw0AcxV/TSkWqgnYQdchQnSyIijhKFYtgobQVWnUwufRDaNKQpLg4Cq4FBz8Wqw4u",
-    "zro6uAqC4AeIm5uToouU+L+k0CLGg+N+vLv3uHsHCPUyU83AOKBqlpGKx8RsbkUMviKAPgQxhB6JmXoivZCB5/i6h4+vd1Ge5X3uz9Gt5E0G+ETiWaYbFvE68fSmpXPeJw6zkqQQnxOPGXRB",
-    "4keuyy6/cS46LPDMsJFJzRGHicViG8ttzEqGSjxFHFFUjfKFrMsK5y3OarnKmvfkLwzlteU012kOI45FJJCECBlVbKAMC1FaNVJMpGg/5uEfdPxJcsnk2gAjxzwqUCE5fvA/+N2tWZiccJNC",
-    "MaDjxbY/RoDgLtCo2fb3sW03TgD/M3CltfyVOjDzSXqtpUWOgN5t4OK6pcl7wOUOMPCkS4bkSH6aQqEAvJ/RN+WA/luga9XtrbmP0wcgQ10t3QAHh8BokbLXPN7d2d7bv2ea/f0AO8NykaOw",
-    "p3oAAAAGYktHRAAAAK0A+eyCP/QAAAAJcEhZcwAALiMAAC4jAXilP3YAAAAHdElNRQflBBIHHAkfKeo5AAAAGXRFWHRDb21tZW50AENyZWF0ZWQgd2l0aCBHSU1QV4EOFwAAAAxJREFUCNdj",
-    "YKATAAAAaQABwB3y+AAAAABJRU5ErkJggg==",
-];
-
 export default async function Accessory(bridge: string, id: string): Promise<{ [key: string]: any }> {
     const results = (await Request.get(`${Config.host.get()}/accessory/${bridge}/${id}`, { headers: { authorization: Config.token.authorization } })).data || {};
 
@@ -48,10 +39,10 @@ export default async function Accessory(bridge: string, id: string): Promise<{ [
             },
         };
 
-        results.snapshot = async (): Promise<string> => {
+        results.snapshot = async (): Promise<string | undefined> => {
             const { image } = (await Request.get(`${Config.host.get()}/accessory/${bridge}/${id}/snapshot`, { headers: { authorization: Config.token.authorization } })).data;
 
-            if (!image) return BLANK_IMAGE.join("");
+            if (!image) return undefined;
 
             return image;
         };
